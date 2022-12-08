31e048726429da7ac1e008f98ff08d8448fddbab
pedrog01 <pedro7109.j@gmail.com>
2022-12-08 12:13:23 -0300

    Array Map 3

---
+++ map3.js
@@ -0,0 +1,23 @@
+Array.prototype.map2 = function(callback){
+    const newArray = []
+    for(let i= 0; i<this.length; i++) {
+        newArray.push(callback(this[1], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45 }',
+    '{"nome": "Caderno", "preco": 13.90 }',
+    '{"nome": "Kit de Lapis", "preco": 41.22 }',
+    '{"nome": "Caneta", "preco": 7.50 }',
+]
+
+
+// Retornar um array apenas com os preços
+const paraObjt = json => JSON.parse(json) //JSON.parse converte String para objeto
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjt).map(apenasPreco)
+
+console.log(resultado)
